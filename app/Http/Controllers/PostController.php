1cc76beb58cdf05f106cf3bc6007682ba17de9d4
tuncerdamat <tuncer.damat@gmail.com>
2024-04-16 18:29:58 +0200

    - Created Post entity, migration and factory - Creates and lists the posts - Handles relationship between user and Post entities

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::get();   // Collection
+        
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     public function store(Request $request)
     {
-        dd('ok');
+        $this->validate($request, [
+           'body' => 'required' 
+        ]);
+        
+        auth()->user()->posts()->create($request->only('body'));
+        
+        return back();
     }
 }
