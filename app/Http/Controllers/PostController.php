57a970c699cdfb08ca35ca5b9cc60807efd05ff9
tuncerdamat <tuncer.damat@gmail.com>
2024-04-19 15:24:55 +0200

    - Solve N+1 queries problem by adding relationships user and likes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(2);   // Collection
+        $posts = Post::with(['user', 'likes'])->paginate(2);   // Collection
         
         return view('posts.index', [
             'posts' => $posts
